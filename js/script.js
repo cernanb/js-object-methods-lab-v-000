996df4938a7411c1341dd669a5926016ea4a7322
cernanb <cernanb@gmail.com>
2016-05-08 19:04:49 -0700

    created presidents

--- js/script.js
+++ js/script.js
@@ -19,3 +19,8 @@ function President(name, politicalParty, yearsInOffice, homeState){
     return "Hi, my name is " + this.name + ". I am from " + this.homeState + ". I represent the " + this.politicalParty + "s, and was in office " + this.yearsInOffice + ".";
   };
 }
+
+washington = new President("George Washington", "Independent Party", "1789-1797", "Virginia");
+lincoln = new President("Abraham Lincoln", "National Union Party", "1861-1865", "Kentucky");
+nixon = new President("Richard Nixon", "Republican Party", "1969-1974", "California");
+carter = new President("Jimmy Carter", "Democratic Party", "1977-1981", "Georgia");
